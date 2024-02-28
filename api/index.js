0f12f924e5ca955cdcc3bf4e0dd81d17b7c6a753
Sujeeththecoder <sujeethgorati08@gmail.com>
2024-02-28 21:23:51 +0530

    404 not found

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import  dotenv from 'dotenv';
+import {UserRouter} from './routes/user.route.js';
+import { authrouter } from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=> {
     console.log('connected to mongodb');
@@ -12,3 +14,12 @@ const app = express();
 app.listen(3000, () => {
     console.log('server is running on port 3000');
 });
+
+app.get('/', (req,res)=>{
+    res.json({
+        message: 'Hello  World',
+    });
+});
+
+app.use("/api/user", UserRouter);
+app.use("/api/user", authrouter);
