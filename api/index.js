e2483e4e7e8a10cebbb20d4574601790e378a6dc
Sujeeththecoder <sujeethgorati08@gmail.com>
2024-03-01 19:07:22 +0530

    middleware function to handle errors

--- api/index.js
+++ api/index.js
@@ -25,11 +25,18 @@ app.listen(3000, () => {
     console.log('server is running on port 3000');
 });
 
-app.get('/', (req,res)=>{
-    res.json({
-        message: 'Hello  World',
-    });
-});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err. statusCode || 500;
+    const message = err.message || 'Internal server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
+
