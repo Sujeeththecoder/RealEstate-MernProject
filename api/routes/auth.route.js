810143eb54808f97abc0189812bc53c84799d2c9
Sujeeththecoder <sujeethgorati08@gmail.com>
2024-03-31 22:32:26 +0530

    get user listings api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { google, signin, signup } from '../controllers/auth.controller.js';
 
-const authRouter = express.Router(); // Rename the router variable
+const authRouter = express.Router(); 
 authRouter.post("/signup", signup);
 authRouter.post("/signin", signin);
 authRouter.post('/google', google);
