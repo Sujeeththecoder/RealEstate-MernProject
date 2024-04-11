1f34bca695ed182cf76d9280d79599db50b3cba4
Sujeeththecoder <sujeethgorati08@gmail.com>
2024-04-11 17:03:26 +0530

    fixed Listing import statements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 export default function App() {
   return <BrowserRouter>
   <Header />
@@ -19,7 +20,7 @@ export default function App() {
     <Route path='/about' element={<About />} />
     {/* <Route path='/search' element={<Search />} />
     <Route path='/listing/:listingId' element={<Listing />} /> */}
-    <Route path='/listing/:listingId' element={<Listing />} />
+    <Route path='/listing/:listingId' element={<Listing/>} />
 
 
     <Route element={<PrivateRoute />}>
